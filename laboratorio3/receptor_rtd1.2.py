5718a9544ff4ef2eb018b3fef072f8b93c900851
myriansosa <myriansosa-1972@hotmail.com>
2019-10-09 00:35:20 -0300

    lab3 receptor

--- laboratorio3/receptor_rtd1.2.py
+++ laboratorio3/receptor_rtd1.2.py
@@ -1,26 +1,24 @@
 from constants import *
+from packet import *
+from constants import *
+from network import *
 
 
-def create_socket():
+def create_socket(address, port):
 	UDPsocket = socket(AF_INET, SOCK_DGARM)
-	UDPsocket.bin((RECEIVER_IP, RECEIVER_PORT))
+	UDPsocket.bin((address, port))
 	return UDPsocket
 
 def extract(packet):
 	data = packet.get_data()
 	return data
 
-
-def deliver_data(message):
-	print(message)
-
-
 def rdt_rcv(sock):
-	data = sock.recv(2048)
-	paquete = loads(data)
-	print(paquete)
-	return paquete
+	packet = loads(sock.recv(2048))
+	return packet
 
+def deliver_data(data):
+	print(data)
 
 def close_socket(socket, signal, frame):
 	print ("\rCerrando socket")
@@ -30,7 +28,7 @@ def close_socket(socket, signal, frame):
 
 if __name__ == "__main__":
 	# Creamos el socket "receiver"
-	servidor = create_socket()
+	receptor = create_socket(RECEIVER_IP, RECEIVER_PORT)
 	# Registramos la senial de salida
 	signal.signal(signal.SIGINT, partial(close_socket, receptor))
 	# Imprimimos el cartel "Listo para recibir mensajes..."
@@ -38,10 +36,10 @@ if __name__ == "__main__":
 	# Iteramos indefinidamente
 	while True:
 		# Recibimos un paquete de la red
-		paquete = rdt_rcv(receptor)
-		data = extrac(paquete)
+		packet = rdt_rcv(receptor)
+		data = extrac(packet)
 		deliver_data(data)
-	close_socket(receptor)
+	close_socket(servidor)
 		
 		# Extraemos los datos
 		
