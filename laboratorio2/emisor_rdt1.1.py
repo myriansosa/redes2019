66188aa28f71f36bff234e18aecf23987a5bd7c5
myriansosa <myriansosa-1972@hotmail.com>
2019-10-09 00:31:32 -0300

    lab2 emisor

--- laboratorio2/emisor_rdt1.1.py
+++ laboratorio2/emisor_rdt1.1.py
@@ -1,36 +1,34 @@
 from socket import *#importa toda la libreria de socket
 from contanstes import *
 from pickle import*
+from paquete import*
 
 def create_socket(address, port):
-	servidor=socket(AF_INET, SOCK_DGRAM)
-	servidor.bind((address, port))
-	return servidor
+	UDPsocket=socket(AF_INET, SOCK_DGRAM)
+	return UDPsocket
 	
-def print_message(message):
-    print (message)
 	
-def rdt_send(socket): #recibe paquete del emisor rdt=tranferencia confiable
-    packet= socket.recv(2048) #recibe datos con el buffer asignado 
-    return packet
+def rdt_send(socket): 
+    data= input('ingrese mensaje') 
+    return data.encode('utf-8')
 
-def make_pkt(data):
-	pkt = packet(data,(SOURCE_PORT,RECEIVER_PORT))
+def make_pkt(dato):
+	pkt = packet(SOURCE_PORT,RECEIVER_PORT, dato)
+	return pkt
 	
 def udp_send(socket, pkt):
 	data = dumps(pkt)
 	socket.sendto(data,(RECEIVER_IP, RECEIVER_PORT))
 	
 def close_socket(socket):
-	UDPsocket.close()
+	socket.close()
 	
 		
 if __name__ == "__main__":
-    servidor= create_socket(RECEIVER_IP,SOURCE_PORT)
-    print_message("servidor corriendo")
+    cliente= create_socket()
     while True :
         data=rdt_send(servidor) #recibe paquete del emisor rdt=tranferencia confiable
-        pkt=make_pkt(data)#crea el paquete en la capa de transporte
-        udp_send(emisor, pkt)
-    close_socket(servidor)
+        pkt=make_pkt(dato)#crea el paquete en la capa de transporte
+        udp_send(cliente, pkt)
+    close_socket(cliente)
    
