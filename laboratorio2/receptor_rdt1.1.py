67dccb6adc9cad9891a9e3c76ac06ee8b1129731
myriansosa <myriansosa-1972@hotmail.com>
2019-10-09 00:32:59 -0300

    lab3 receptor

--- laboratorio2/receptor_rdt1.1.py
+++ laboratorio2/receptor_rdt1.1.py
@@ -1,6 +1,7 @@
 from socket import *
 from contanstes import *
 from pickle import*
+from paquete import *
 
 def create_socket(address, port):
 	servidor = socket(AF_INET, SOCK_DGRAM)
@@ -10,17 +11,15 @@ def create_socket(address, port):
 def print_message(message):
     print (message)
 
-def rdt_send():#baja datos desde la capa de aplicaciones
-	data = input('ingrese mensaje: ')
-	return data.encode('utf-8')
 
-
-def udt_rcv(socket):
-	packet = loads(socket.recv(2048))
+def rdt_recv(socket):
+	datos = socket.recv(2048)
+	packet = loads(datos)
 	return packet
 	
-def extract(packet): # estrae el encabezado del paquete, para solo quedarse con los datos
-	return packet
+def extract(packet): 
+	data = packet.get_data()
+	return data
 	
 def deliver_data(data): #envia los datos a la capa de aplicacion
     print (data)
@@ -29,10 +28,10 @@ def close_socket(socket):
     socket.close()
 
 if __name__ == "__main__":
-	emisor=create_socket(RECEIVER_IP, RECEIVER_PORT)
+	servidor=create_socket(RECEIVER_IP, RECEIVER_PORT)
+	print_message('servidor corriendo')
 	while 1:
-		packet=rdt_send()
-		packet=udt_rcv(socket)
-		paquete=extract(packet)
-		data=deliver_data (data)
-	close_socket(cliente)
+		packet = rdt_recv(servidor)
+		data = extract(packet)
+		deliver_data(data)
+	close_socket(servidor)
